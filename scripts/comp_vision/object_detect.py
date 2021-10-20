0dee9abefc6e7a060eb8d06d129528fffdff19f5
Mario <ma.schroed73@gmail.com>
2021-10-20 21:44:04 +0200

    add some doc

--- scripts/comp_vision/object_detect.py
+++ scripts/comp_vision/object_detect.py
@@ -1,7 +1,11 @@
+#
+#trying to detect cars on a video stream
+#
 import cv2
 
 cap = cv2.VideoCapture()
-cap.open('http://192.168.2.100:4747/video')
+#IP cam (e.g. DroidCam)
+cap.open('http://192.168.2.24:4747/video')
 
 car_cascade = cv2.CascadeClassifier('assets/cars.xml')
 
@@ -9,16 +13,16 @@ while True:
     ret, img = cap.read()
     if (type(img) == type(None)):
         break
-    
+
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
+
     cars = car_cascade.detectMultiScale(gray, 1.1, 1)
 
     for (x,y,w,h) in cars:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)      
-    
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+
     cv2.imshow('object detect', img)
-    
+
     if cv2.waitKey(33) == 27:
         break
 
